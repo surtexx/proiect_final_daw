ee95351a2ea5b9c86c94709bab468f1ea6b9dcc7
surtexx <91895868+surtexx@users.noreply.github.com>
2023-01-10 17:03:03 +0200

    Update download_pdf.php

--- download_pdf.php
+++ download_pdf.php
@@ -13,35 +13,62 @@ if(isset($_POST['download_pdf'])){
     $numbers = $_SESSION['numbers'];
     $names = $_SESSION['names'];
     $positions = $_SESSION['positions'];
+    $echipaAleasa = $_SESSION['echipa'];
+
+    unset($_SESSION['numbers']);
+    unset($_SESSION['names']);
+    unset($_SESSION['positions']);
+    unset($_SESSION['echipa']);
+    $url = file_get_contents('https://lpf.ro/cluburi/' . $echipaAleasa);
+    $doc = new \DOMDocument();
+    @$doc->loadHTML($url);
+    $divs = $doc->getElementsByTagName('div');
+    foreach($divs as $div){
+        if($div->hasAttribute('class') && $div->getAttribute('class') == 'club-logo-big'){
+            $mydiv = $div;
+            break;
+        }
+    }
+    $pdf->Image('https://lpf.ro/' . $mydiv->getElementsByTagName('img')->item(0)->getAttribute('src'), 180, 0, 30, 30);
+    $pdf->SetFont('Courier', '', 16);
     $pdf->Cell(40,10,'Portari:');
+    $pdf->SetFont('Courier','',12);
     $pdf->Ln();
     for($i=0;$i<count($names);$i++)
         if($positions[$i] == "Portar"){
             $pdf->Cell(40,10, $numbers[$i] . ' ' . $names[$i]);
             $pdf->Ln();
         }
+    $pdf->SetFont('Courier', '', 16);
     $pdf->Cell(40,10, 'Fundasi:');
+    $pdf->SetFont('Courier','',12);
     $pdf->Ln();
     for($i=0;$i<count($names);$i++)
         if($positions[$i] == "Fundaș"){
             $pdf->Cell(40,10,$numbers[$i] . ' ' . $names[$i]);
             $pdf->Ln();
         }
+    $pdf->SetFont('Courier', '', 16);
     $pdf->Cell(40,10,'Mijlocasi:');
+    $pdf->SetFont('Courier','',12);
     $pdf->Ln();
     for($i=0;$i<count($names);$i++)
         if($positions[$i] == "Mijlocaș"){
             $pdf->Cell(40,10,$numbers[$i] . ' ' . $names[$i]);
             $pdf->Ln();
         }
+    $pdf->SetFont('Courier', '', 16);
     $pdf->Cell(40,10,'Atacanti:');
+    $pdf->SetFont('Courier','',12);
     $pdf->Ln();
     for($i=0;$i<count($names);$i++)
         if($positions[$i] == "Atacant"){
             $pdf->Cell(40,10,$numbers[$i] . ' ' . $names[$i]);
             $pdf->Ln();
         }
+    $pdf->SetFont('Courier', '', 16);
     $pdf->Cell(40,10,'Nespecificat:');
+    $pdf->SetFont('Courier','',12);
     $pdf->Ln();
     for($i=0;$i<count($names);$i++)
         if($positions[$i] == "-"){
