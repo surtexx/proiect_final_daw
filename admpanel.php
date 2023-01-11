c3a75a1d7b21f20a6c2591f7c0de646c2be7f490
surtexx <91895868+surtexx@users.noreply.github.com>
2023-01-11 19:52:20 +0200

    Update admpanel.php

--- admpanel.php
+++ admpanel.php
@@ -29,7 +29,36 @@
 	</nav>
     <br>
     <main>
-        
+        <?php
+			$url = file_get_contents('https://lpf.ro/liga-1');
+			$dom = new \DOMDocument();
+			@$dom->loadHTML($url);
+			$tables = $dom->getELementsByTagName('table');
+			$clasament_table = $dom->createElement('table');
+			foreach($tables as $t_table){
+				if ($t_table->hasAttribute('class') && $t_table->getAttribute('class') == 'clasament_general white-shadow') {
+					$clasament_table = $t_table;
+					break;
+				}
+			}
+			$tds = $clasament_table->getElementsByTagName('td');
+			$trs = $clasament_table->getElementsByTagName('tr');
+			$n_places = 0;
+			$echipe = array();
+			$link_echipe = array();
+			foreach ($tds as $td) {
+				if ($td->hasAttribute('class') && $td->getAttribute('class') == 'echipa'){
+					$n_places += 1;
+					$echipa = $td->getElementsByTagName('a')->item(0)->nodeValue;
+					if(strpos($echipa,'*') !== false)
+						$echipe[] = substr($echipa,0, strlen($echipa)-1);
+					else
+						$echipe[] = $echipa;
+					$splitted_href = explode('/', $td->getElementsByTagName('a')->item(0)->getAttribute('href'));
+					$link_echipe[] = $splitted_href[1] . '/' . $splitted_href[2];
+				}
+			}
+        ?>
         <form action="deleteuser.php" method="post">
             <label for="deleteuser">Introduceți userul pentru a îl șterge: </label>
             <?php if (isset($_GET['deletemessage'])) { ?>
@@ -39,13 +68,21 @@
             <input type="submit" value="Șterge"></input>
         </form>
         <form action="createadminuser.php" method="post">
-            <span>Introduceți un username și o parolă pentru a crea un nou admin: </span><br>
+            <span>Introduceți un username, o parola si o echipa preferata pentru a crea un nou admin: </span><br>
             <?php if (isset($_GET['createadminmessage'])) { ?>
 				<h3><?php echo $_GET['createadminmessage']; ?></h3>
             <?php } ?>
             <input type="text" name="adminusername" id="adminusername" placeholder="Username"><br>
             <input type="password" name="adminpassword" id="adminpassword" placeholder="Password"><br>
             <input type="password" name="admincopypassword" id="admincopypassword" placeholder="Same password"><br>
+				<select name="echipe" id="echipe">
+					<option value="-">-</option>
+					<?php
+						for($i=0;$i<$n_places;$i++){
+							echo "<option value=$link_echipe[$i]>$echipe[$i]</option>";
+						}
+					?>
+				</select>
             <input type="submit" value="Creează admin"></input>
         </form>
 
